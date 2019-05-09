94fbb563af184dbe7828d4bcfb17b23dfaa58d84
CalvinOh <calvinohwyekit@gmail.com>
2019-05-08 19:15:06 -0500

    pause menu working

--- Final Project/ocean-gallery-script.js
+++ Final Project/ocean-gallery-script.js
@@ -1,30 +1,41 @@
 let slideIndex = 0;
+let slidePaused = false;
 showSlides();
 
 function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("slides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-
-  slides[slideIndex-1].style.display = "block";   
-  setTimeout(showSlides, 4000); 
+    if(slidePaused == false) {
+        let i;
+        let slides = document.getElementsByClassName("slides");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}    
+        slides[slideIndex-1].style.display = "block";   
+        setTimeout(showSlides, 4000); 
+    }
+  
 }
 
 document.body.addEventListener("keydown", (e) => {
- 
     pauseSlideShow();
    
 });
 
 
 function pauseSlideShow(){
-    if(event.keyCode == 32) {
-        document.getElementById("pause").classList.add(".pauseMenu");
-        setTimeout(showSlides, 0);
+    if(event.keyCode == 80 && slidePaused == false) {
+        document.getElementById("pause").style.display = "block";
+        document.getElementById("pause").classList.toggle("pauseMenu");
+        slidePaused = true;
+        console.log("Pausing SlideShow");
+    }
+    else if(event.keyCode == 80 && slidePaused == true) {
+        document.getElementById("pause").style.display = "none";
+        document.getElementById("pause").classList.toggle("pauseMenu");
+        slidePaused = false;
+        console.log("Resuming SlideShow");
+        showSlides();
     }
     
 }
